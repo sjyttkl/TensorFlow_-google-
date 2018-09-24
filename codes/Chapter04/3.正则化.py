b13a98cbd18e08bd9b111442ca44bf5cf09028c0
sjyttkl <695492835@qq.com>
2018-09-25 00:31:23 +0800

    正则化学习

---
+++ codes/Chapter04/3.正则化.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+===============================================================
+author：songdongdong
+email：songdongdong@jd.com
+date：2018.09.25
+introduction:正则化
+===============================================================
+"""
+__author__ = "songdongdong"
+#1. 生成模拟数据集。
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+data = []
