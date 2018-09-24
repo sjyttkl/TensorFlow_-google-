5388602173ad05f02299df96df48b880d304ceea
sjyttkl <695492835@qq.com>
2018-09-25 00:31:47 +0800

    这个，就是个草稿，demo

--- codes/Chapter04/ex.py
+++ codes/Chapter04/ex.py
@@ -55,3 +55,13 @@ learning_rate = tf.train.exponential_decay(0.1,global_step,100,0.96,staircase=Tr
 
 #使用指数函数的学习率，在minimize函数中传入global_step将自动更新
 # learning_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
+#=============================正则化====================
+# w = tf.Variable(tf.random_normal([2,1],stddev=1,seed=1))
+# y = tf.matmul(x,w)
+# loss = tf.reduce_mean(tf.square(y_ - y))+tf.contrib.layers.12_regularizer(lambda )(w)
+weights = tf.constant([[1.0,-2.0],[-3.0,4.0]])
+with tf.Session() as sess:
+    #输出为：（1 -2-3+4）*0.5
+    print(sess.run(tf.contrib.layers.l1_regularizer(.5)(weights)))
+    #输出为：(1^2+(-2)^2+(-3)^2+(4)^2) = 7.5 #Tensorflow 会把L2正则化除以2使得求导结果更加简洁
+    print(sess.run(tf.contrib.layers.l2_regularizer(.5)(weights)))
