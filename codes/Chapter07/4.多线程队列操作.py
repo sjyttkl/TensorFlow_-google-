b70f8e5695ef3e6300b10e9ba04e55fd7eb34516
songdongdong2 <songdongdong@jd.com>
2019-03-24 00:56:23 +0800

    多线程队列操作

--- codes/Chapter07/4.多线程队列操作.py
+++ codes/Chapter07/4.多线程队列操作.py
@@ -15,14 +15,16 @@ import tensorflow as tf
 #1.定义队列以及操作
 queue = tf.FIFOQueue(100,"float")
 enqueue_op = queue.enqueue([tf.random_normal([1])])
-qr = tf.train.QueueRunner(queue,[enqueue_op]*5)
-tf.train.add_queue_runner(qr)
+qr = tf.train.QueueRunner(queue,[enqueue_op]*5)#用来创建多个线程运行队列的入队操作
+tf.train.add_queue_runner(qr)#将刚刚定义的qr加入默认的tf.GraphKeys.QUEUE_RUNNERS集合
+#定义出队操作
 out_tensor = queue.dequeue()
 
 #2. 启动线程。
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess,coord=coord)
+    threads = tf.train.start_queue_runners(sess=sess,coord=coord)#用来启动所有线程。否则会因为没有线程运行入队操作，当调用出队操作时候，
+    #程序会一直等待入队。
     for _ in range(3):
         print(sess.run(out_tensor)[0])
     coord.request_stop()
